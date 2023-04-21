35dd938d59dff6b90814dc84fe3b2440b1fd3aeb
jonathan <jo@DESKB.localdomain>
2023-04-21 02:26:30 +0200

    1st

---
+++ philo_2/include/philo_2.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_3.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jo <jo@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 01:53:08 by jo                #+#    #+#             */
+/*   Updated: 2023/04/21 01:53:11 by jo               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+
+// LIBRAIRIE
+# include   	<string.h>
+# include       <stdio.h>
+# include       <stdlib.h>
+# include       <unistd.h>
+# include       <sys/time.h>
+# include       <pthread.h>
+
+//Struct
+
+//Main function 
+
+//Utils
+
+#endif
